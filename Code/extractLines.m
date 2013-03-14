7a1516e0b6f34d5cc0806070de2090c0d14b59ca
thorvaldurhelga <thorvaldurhelga@gmail.com>
2013-03-14 14:09:33 +0000

    The extractLines function now returns row and column information on both the linePoints that were detected with RANSAC and the lines fitted through them

--- Code/extractLines.m
+++ Code/extractLines.m
@@ -1,32 +1,40 @@
-function [frl,fcl,cr,cc] = extractLines(edges, ransacIterations, showPlots)
-r = {};
-c = {};
-for i = 1:size(edges,2)
-	[r{i},c{i}] = find(edges{i}==1);
-    
+function [pointRows,pointCols,lineRows,lineCols] = extractLines(edges, ransacIterations, showPlots)
+%DETECTEDGES A pre-processing function that performs canny edge detection and removes irrelevant background information for the frame provided as input
+% INPUT: @edges - Binary image containing only edges in frame that is
+%        outputted by detectEdges.m
+%        @ransacIterations - Number of iterations run to fit lines to edges
+%        in RANSAC 
+%        @showPlots - Boolean indicating whether to plot the edges and
+%        lines or not
+% OUTPUT: @pointRows - Row information for the line-edge overlap points
+%         @pointCols - Column information for the line-edge overlap points
+%         @lineRows - Row information for the fitted line
+%         @lineCols - Column information for the fitted lines
+
+	[rows,cols] = find(edges==1);   
     if(showPlots == true)
-        figure(i);
-        plot(c{i},r{i},'k.');
+        figure(1);
+        plot(cols,rows,'k.');
         axis([0 640 0 480]);
         axis ij;
         hold on;
     end;
-        
+    
+    pointRows = {};
+    pointCols = {};
+    lineRows = {};
+    lineCols = {};
+    
 	% RANSAC
-	flag = 1;
 	for j = 1:ransacIterations
 		j
-		[flag,t,d,nr,nc,count,frl,fcl,newcountl] = ransacline(r{i},c{i},1,0.1,0.01,0.001,140,3);
-        plot(fcl,frl,'r.');
-		hold on;
-		if flag == 1 & newcountl > 0 
-			[cr,cc] = plotline(t,d);
-			plot(cc,cr);
-            size(cc);
-            size(cr);
+		[flag,t,d,nr,nc,count,pointRows{j},pointCols{j},pointCount] = ransacline(rows,cols,1,0.1,0.01,0.001,140,3);
+        if pointCount > 0
+            plot(pointCols{j},pointRows{j},'r.');
             hold on;
-		end;
+            [lineRows{j},lineCols{j}] = plotline(t,d);
+			plot(lineCols{j},lineRows{j});
+            hold on;
+        end;
 	end;
-	pause(0.5);
-end;
-  
+end
