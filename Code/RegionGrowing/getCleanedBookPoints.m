480227ebbd0d1d5e777ce241437f958ccfb6ff64
hyweldd <hyweldd@googlemail.com>
2013-03-20 21:51:54 +0000

    Completed region growing code to remove noise from book points

--- Code/RegionGrowing/getCleanedBookPoints.m
+++ Code/RegionGrowing/getCleanedBookPoints.m
@@ -1,26 +1,33 @@
 % getCleanedBookPoints:
 
-function cleanedBookPoints = getCleanedBookPoints(bookPoints,remainingPoints)
+function cleanedBookPoints = getCleanedBookPoints(bookPoints)
 
-	fig = figure(1);
-	clf
-	hold on
-	plot3(bookPoints(:,1),bookPoints(:,2),bookPoints(:,3),'k.')
+	cleanedBookPoints = {};
 
+	% step through each book point cloud filtering out non-book points
+	for i = 1:21
 
-	
+		fig = figure(1);
+		clf
+		hold on
+		plot3(bookPoints{i}(:,1),bookPoints{i}(:,2),bookPoints{i}(:,3),'k.')
 
+		% Calculate the highest book point - this will be the seed point - the idea is that we're resonably sure that this will be a true book point
+		highestBookPoint = bookPoints{i}(find(bookPoints{i}(:,2)==max(bookPoints{i}(:,2))),:);
 
+		% Find the true book points
+		regionGrowingInput{1} = highestBookPoint;
+		regionGrowingInput{2} = [];
+		regionGrowingInput{3} = bookPoints{i};
+		regionGrowingInput{4} = 1;
 
+		cleaningOutput = growCleanedBookPoints(regionGrowingInput);
+		cleanedBookPoints{i} = cleaningOutput{2};
 
+		% plot the results
+		plot3(cleanedBookPoints{i}(:,1),cleanedBookPoints{i}(:,2),cleanedBookPoints{i}(:,3),'r.')
+		pause(1);
 
-
-
-
-
-	cleanedBookPoints = bookPoints;
-
-	plot3(cleanedBookPoints(:,1),cleanedBookPoints(:,2),cleanedBookPoints(:,3),'y.')
-
+	end
 
 end
