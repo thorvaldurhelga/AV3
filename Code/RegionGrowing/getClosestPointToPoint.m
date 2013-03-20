2d0ca12796a9bd3366b660eebf8a7ea22befdf90
thorvaldurhelga <thorvaldurhelga@gmail.com>
2013-03-20 17:12:00 +0000

    import/export closest points

--- Code/RegionGrowing/getClosestPointToPoint.m
+++ Code/RegionGrowing/getClosestPointToPoint.m
@@ -7,7 +7,7 @@ l = length(pointCloud(:,1));
 
 dist = abs(pointCloud(:,1)-point(1))+abs(pointCloud(:,2)-point(2))+abs(pointCloud(:,3)-point(3));
 
-closestIndex = find(dist==min(dist))
+closestIndex = find(dist==min(dist));
 
 closestPoint = pointCloud(closestIndex(1),:);
 
